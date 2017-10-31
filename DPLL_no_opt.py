b79c42d569f3978e0d4e92f5faf5d5238ab41de2
dkegle <domenkeglevic@gmail.com>
2017-10-31 17:46:16 +0100

    For clauses use set instead of list

--- DPLL_no_opt.py
+++ DPLL_no_opt.py
@@ -1,51 +1,60 @@
+import time
+
 def dimacs_to_list(file_name):
     with open(file_name) as file:
-        return [[int(var) for var in line.split()[:-1]]
-                for line in file if line[0] != 'c' and line[0] != 'p']
+        return [set([int(var) for var in line.split()[:-1]]) \
+            for line in file if line[0] != 'c' and line[0] != 'p']
 
 
-def unit_clouse(clouses):
-    for l in clouses:
-        if len(l) == 1:
-            return l[0]
+def unit_clause(clauses):
+    """ return literal from some unit clause, if it exists """
+    for clause in clauses:
+        if len(clause) == 1:
+            return next(iter(clause))
     return None
 
 
-def simplify(clouses, l):
-    return [[lit for lit in cl if lit != -l] for cl in clouses if l not in cl]
+def simplify(clauses, l):
+    """ remove clauses with literal l, and remove -l from remaining literals """
+    new_clauses = []
+    for clause in clauses:
+        if l not in clause:
+            clause.discard(-l)
+            new_clauses.append(clause)
+    return new_clauses
 
 
-def DPLL(clouses, val):
+def DPLL(clauses, val):
 
-    # step 1: filter unit clouses
-    l = unit_clouse(clouses)
+    # step 1: filter unit clauses
+    l = unit_clause(clauses)
 
     while l:
         val.append(l)
-        clouses = simplify(clouses, l)
-        l = unit_clouse(clouses)
+        clauses = simplify(clauses, l)
+        l = unit_clause(clauses)
 
     # step 2: filter pure literals - skip
 
     # step 3: have we finished?
-    if not clouses:
+    if not clauses:
         return val
-    if [] in clouses:
+    if set() in clauses:
         return None
 
     # step 4: make assumptions l or not l
-    l = clouses[0][0]
-    val_l = DPLL(simplify(clouses, l), val.append(l))
+    l = next(iter(clauses[0]))
+    val_l = DPLL(simplify(clauses, l), val.append(l))
     if val_l:
-        return l
+        return val_l
     else:
-        return DPLL(simplify(clouses, -l), val.append(-l))
+        return DPLL(simplify(clauses, -l), val.append(-l))
 
 
-def test(clouses, solution):
-    for clouse in clouses:
+def test(clauses, solution):
+    for clause in clauses:
         sat = False
-        for literal in clouse:
+        for literal in clause:
             if literal in solution:
                 sat=True
                 break
@@ -55,6 +64,15 @@ def test(clouses, solution):
 
 
 # tests
+
+def test_run(file):
+    CNF = dimacs_to_list(file)
+    start_time = time.time()
+    solution = DPLL(CNF, [])
+    print("Time needed " + str(time.time() - start_time) + " s")
+    print(test(CNF, solution))
+
+
 '''
 CNF = dimacs_to_list('sudoku1.txt')
 solution = DPLL(CNF, [])
@@ -62,6 +80,8 @@ print(solution, test(CNF, solution))
 
 CNF2 = dimacs_to_list('sudoku2.txt')
 solution2 = DPLL(CNF2, [])
-print(test(CNF, solution2))
+print(test(CNF2, solution2))
+
 '''
 
+# test_run('sudoku2.txt')
