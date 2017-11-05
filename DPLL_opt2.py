3fc925a7974790d69d5887b0d032bf2ef56c6c86
dkegle <domenkeglevic@gmail.com>
2017-11-05 10:43:15 +0100

    DPLL with loop instead of recursion

--- DPLL_opt2.py
+++ DPLL_opt2.py
@@ -15,76 +15,109 @@ def get_unit_clauses(clauses):
     for clause in clauses:
         if len(clause) == 1:
             unit_clauses.add(next(iter(clause))) # first element
-    return [c for c in unit_clauses]
+    return unit_clauses
 
 
 def simplify(clauses, unit_clauses):
-    """ skip clauses with literals from unit clauses, remove -l from remaining
-        and return deep copy """
-    new_clauses = []
-    new_unit_clauses = []
-    neg_clauses = [-l for l in unit_clauses] # this should be a lazy evaluation
-    for clause in clauses:
-        for literal in unit_clauses:
-            if literal in clause:
-                break
-        else:   # if no literal from unit_clauses is in clause
-            new_clause = copy.deepcopy(clause)
-            new_clause = new_clause.difference(neg_clauses)
-            if len(new_clause) == 1:
-                new_unit_clauses.append(next(iter(new_clause)))
-            else:
-                new_clauses.append(new_clause)
-    return new_clauses, new_unit_clauses
-
-
-def simplify_old(clauses, unit_clauses):
-    """ skip clauses with literals from unit clauses, remove -l from remaining
+    """ skip clauses with literals from unit clauses, remove -l from remaining 
         and return deep copy """
     new_clauses = []
     neg_clauses = [-l for l in unit_clauses]
     for clause in clauses:
+        if len(clause) == 1:    # skip unit clauses
+            continue
+        if len(clause) == 0:    # contradiction
+            return [set()]
         for literal in unit_clauses:
             if literal in clause:
                 break
         else:   # if no literal from unit_clauses is in clause
             new_clause = copy.deepcopy(clause)
             new_clause = new_clause.difference(neg_clauses)
+            if len(new_clause) == 0: # found contradiction
+                return [set()]
             new_clauses.append(new_clause)
     return new_clauses
 
+class LocalEnvironment:
+    def __init__(self, clauses):
+        # parameters
+        self.clauses = clauses
+        self.val = set()
+        # local vars
+        self.clauses_new = []
+        self.unit_clauses = set()
+        self.val_new = set()
+        self.l = 0
+        self.next_step = 0
 
-def DPLL(clauses, val):
 
-    # step 1: filter unit clauses
-    unit_clauses = get_unit_clauses(clauses)
-    val_new = copy.deepcopy(val)
-    while len(unit_clauses) > 0:
-        val_new.update(unit_clauses)
-        clauses, unit_clauses = simplify(clauses, unit_clauses)
-        # unit_clauses = get_unit_clauses(clauses)
+def DPLL(cnf):
 
-    # step 2: filter pure literals - skip
+    env = LocalEnvironment(cnf)
+    stack = [env]
 
-    # step 3: have we finished?
-    if not clauses:
-        return val_new
-    if set() in clauses:
-        return None
+    while True:
+        
+        if env.next_step == 0:
+            # step 1: filter unit clauses
+            env.unit_clauses = get_unit_clauses(env.clauses)
+            env.val_new = copy.deepcopy(env.val)
 
-    # step 4: make assumptions l or not l
-    l = next(iter(clauses[0]))
-    val_new.add(l)
-    new_clauses = simplify_old(clauses, [l])
-    val_l = DPLL(new_clauses, val_new)
-    if val_l:
-        return val_l
-    else:
-        val_new.discard(l)
-        val_new.add(-l)
+            contradiction = False
+            while len(env.unit_clauses) > 0:
+                for l in env.unit_clauses: # check for contradictions
+                    if -l in env.unit_clauses:
+                        if not stack: # if nothing on stack, return immediately
+                            return None
+                        else:   # backtrack
+                            contradiction = True
+                            env = stack.pop()
+                            break
+                if contradiction:
+                    break
+                env.val_new.update(env.unit_clauses)
+                env.clauses = simplify(env.clauses, env.unit_clauses)
+                env.unit_clauses = get_unit_clauses(env.clauses)
 
-        new_clauses = simplify_old(clauses, [-l])
-        return DPLL(new_clauses, val_new)
+            if contradiction:
+                continue
+
+            # step 2: filter pure literals - skip
+
+            # step 3: have we finished?
+            if not env.clauses:
+                return env.val_new
+            if set() in env.clauses:
+                if not stack:
+                    return None
+                else:
+                    env = stack.pop()
+                    continue
+
+            # step 4: make assumptions l or not l
+            env.l = next(iter(env.clauses[0]))
+            env.val_new.add(env.l)
+            env.clauses_new = simplify(env.clauses, [env.l])
+
+            env.next_step = 1
+
+            cp_env = copy.deepcopy(env)
+            stack.append(cp_env)
+            env.clauses = env.clauses_new
+            env.val = env.val_new
+            env.next_step = 0   # recurse
+            
+        elif env.next_step == 1:
+            env.val_new.discard(env.l)
+            env.val_new.add(-env.l)
+            env.clauses_new = simplify(env.clauses, [-env.l])
+
+            env.next_step = 0
+            env.clauses = env.clauses_new
+            env.val = env.val_new
+
+    return None
 
 
 def test(clauses, solution):
@@ -108,22 +141,14 @@ def test(clauses, solution):
 def test_run(file):
     CNF = dimacs_to_list(file)
     start_time = time.time()
-    solution = DPLL(CNF, set())
+    solution = DPLL(CNF)
     print("Time needed " + str(time.time() - start_time) + " s")
     print(test(CNF, solution))
 
 
-'''
-CNF = dimacs_to_list('sudoku1.txt')
-solution = DPLL(CNF, [])
-print(solution, test(CNF, solution))
-CNF2 = dimacs_to_list('sudoku2.txt')
-solution2 = DPLL(CNF2, [])
-print(test(CNF2, solution2))
-'''
+#for filename in os.listdir('test_data\\TESTS1_satisfiable'):
+#    print("\n"+filename)
+#    test_run('test_data\\TESTS1_satisfiable\\' + filename)
+    
+#test_run("xdata\\sudoku_hard.txt")
 
-
-
-for filename in os.listdir('TESTS1_satisfiable'):
-    print("\n"+filename)
-    test_run('TESTS1_satisfiable\\'+filename)
